46ba0da5b52110efd28dcfd3653b47fd106c7d86
Andy Situ <and.situ@gmail.com>
2015-08-15 21:01:43 -0700

    up

---
+++ mainGameScreen.js
@@ -0,0 +1,28 @@
+var mainGameScreen (function() {
+
+	// set of entities being updated and rendered
+	var entities = [];
+	// how many enemy ships want to start with
+	var numOfEnemyShips = 12;
+
+	// initialize the screen, expected to be called
+	// once transition to the screen
+	function start() {
+		for (var i = 0; i < numOfEnemyShips; i++) {
+			entities.push(makeEnemyShip(i * 10, i));
+		}
+	}
+
+	function draw(ctx) {
+
+		ctx.fillStyle = 'black';
+		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+		// delegate drawing of entitye to itself
+		var entityIndex = entities.length - 1;
+
+		for ( ; entityIndex != 0; entityIndex--) {
+			entities[entityIndex].draw(ctx);		
+		}
+	}
+})
