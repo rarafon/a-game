2f7c1df9ce57b7538895dce38023ca44649a25b3
Andy Situ <and.situ@gmail.com>
2015-08-15 18:29:42 -0700

    startScreen
    * http://dev.bennage.com/blog/2013/01/11/game-dev-02/

---
+++ startScreen.js
@@ -0,0 +1,83 @@
+var startScreen = (function(input) {
+  
+  // red component of rgb
+  var hue = 0;
+
+  // direction towards red or black
+  var direction = 1;
+  var transitioning = false;
+
+  // record last input state from last frame to compare
+  // to current
+  var wasButtonDown = false;
+
+  // helper function to draw text into the
+  // center of the canvas with respect to x coord
+  function centerText(ctx, text, y) {
+  	var measurement = ctx.measureText(text);
+  	var x = (ctx.canvas.width - measurement.width) / 2;
+  	ctx.fillText(text, x, y);
+  }
+
+  // draw main menu to canvas
+  function draw(ctx, elapsed) {
+
+  	// draw text in middle of canvas.
+  	// inefficient, take out later
+  	var y = ctx.canvas.height / 2;
+
+  	// css color from 'hue'
+  	var color = 'rgb(' + hue + ',0,0)';
+
+  	// clear entire canvas, not really necessary
+  	ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+
+  	// draw title of game, static
+  	ctx.fillStyle = 'white';
+  	ctx.font = '48px monospace';
+  	centerText(ctx, 'My Awesome Game', y);
+
+  	// draw instructions to player
+  	ctx.fillStyle = color;
+  	ctx.font = '24px monospace';
+  	centerText(ctx, 'clickto begin', y + 30);
+  }
+
+  // update color & check for input from user
+  function update() {
+  	// want hue to oscillate betw 0 & 255
+  	hue += 1 * direction;
+  	if (hue > 255) {
+  		direction = -1;
+  	} else if (hue < 0) {
+  		direction = 1;
+  	}
+
+  	// magically capturing state of game &
+  	// not dealing with events inside game loop
+  	// will come back to it later(?)
+  	var isButtonDown = input.isButtonDown();
+
+  	// want to know if input (mouse click) just happened
+  	// want to transition from menu to game if there was
+  	// input from last frame but none on current one.
+  	var mouseJustClicked = !isButtonDown && wasButtonDown;
+
+  	// want to check value of transition so transition logic
+  	// won't be inititated more than once (ex. player clicking mouse
+  	// mouse repeatedly)
+	if (mouseJustClicked && !transitioning) {
+		transitioning = true;
+		// do something here to transition game
+	}
+
+	// record state of input for use in the next frame
+	wasButtonDown = isButtonDown;
+
+	// this is the obj that will be 'startScreen'
+	return {
+		draw: draw,
+		update: update
+	};
+  }
+}());
